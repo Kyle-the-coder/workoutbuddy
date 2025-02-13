7e838ec4fe251c93c62b9a233921cdb5d437d653
Kyle-the-coder <kylethacoder@gmail.com>
2025-02-13 11:19:35 -0700

    updated

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const router = require("./routes/workouts");
 
 const app = express();
@@ -16,7 +17,15 @@ app.use((req, res, next) => {
 //routes
 app.use("/api/workouts", workoutRoutes);
 
-//listen for requests
-app.listen(process.env.PORT, () => {
-  console.log("listening on port", process.env.PORT);
-});
+//Connect to DB
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    //listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log("connected to db and listening on port", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
