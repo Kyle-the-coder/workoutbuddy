e5978e6e9df3ee195ed218995f027edeb205c557
Kyle-the-coder <kylethacoder@gmail.com>
2025-02-17 10:44:36 -0700

    updated

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,17 @@
+import { useEffect, useState } from "react";
 export function Home() {
+  const [workouts, setWorkouts] = useState(null);
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch("http://localhost:5173//api/workouts");
+      const json = await response.json();
+
+      if (response.ok) {
+        setWorkouts(json);
+      }
+    };
+    fetchWorkouts();
+  }, []);
   return (
     <div className="home">
       <h1>home</h1>
